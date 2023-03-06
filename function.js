6d1a78310912bb48980bf21c3e5768221d4bcade
Jonathan A. Montero C <dyatroniccr@gmail.com>
2023-03-06 05:04:35 +0000

    Update Final del Proyecto

--- function.js
+++ function.js
@@ -40,7 +40,7 @@ function numberIsDivisible(num) {
   if (num <= 0) {
     return "Number is not positive";
   }
-  if (num % 2 === 0) && if (num % 3 === 0) {
+  if (num % 2 === 0) && if(num % 3 === 0) {
     return "Divisible by 2 and 3";
   } 
   if (num % 2 === 0) {
