eda8f3d55dc6a25526e959965cf4030ee7140348
Jonathan A. Montero C <dyatroniccr@gmail.com>
2023-03-06 04:35:04 +0000

    Final del Proyecto

---
+++ function.js
@@ -0,0 +1,21 @@
+function isOldEnoughToDrive(age) {
+  if (age > 16) return "Yes"
+  return "No";
+}
+
+
+function numberIsDivisible(num) {
+  if (num <= 0) {
+    return "Number is not positive";
+  }
+  if (num % 2 === 0) && if (num % 3 === 0) {
+    return "Divisible by 2 and 3";
+  } 
+  if (num % 2 === 0) {
+    return "Divisible by 2";
+  }
+  if (num % 3 === 0) {
+    return "Divisible by 3";
+  }   
+  return "Not divisible by 2 or 3";
+}
